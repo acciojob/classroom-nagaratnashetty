380cd2f262728c10c392954e1b4b788204f6a81c
nagaratnashetty <125356022+nagaratnashetty@users.noreply.github.com>
2024-04-05 18:37:11 +0530

    Update Student.java

--- src/main/java/com/driver/Student.java
+++ src/main/java/com/driver/Student.java
@@ -1,39 +1,34 @@
 package com.driver;
 
+import net.bytebuddy.asm.Advice;
+
 public class Student {
 
     private String name;
     private int age;
     private double averageScore;
 
-    public Student(){
-
-    }
-
     public Student(String name, int age, double averageScore) {
         this.name = name;
         this.age = age;
         this.averageScore = averageScore;
     }
-
-    public String getName() {
+    public String getName(){
         return name;
+    }
+    public int getAge(){
+        return age;
+    }
+    public double getAverageScore() {
+        return averageScore;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
-    }
-
     public void setAge(int age) {
         this.age = age;
-    }
-
-    public double getAverageScore() {
-        return averageScore;
     }
 
     public void setAverageScore(double averageScore) {
